cebc6efe69c94215f1f16d86b5c70f292d34afca
wdt163 <cj163wdt@163.com>
2021-03-12 14:38:39 +0800

    在idea中测试git提交

---
+++ src/com/cj/Test.java
@@ -0,0 +1,11 @@
+package com.cj;
+
+public class Test {
+    public static void main(String[] args) {
+        String str="wdt："+123;
+        System.out.println(str);
+        System.out.println(4&7);
+        char[] c=new char[]{1,2,3};
+
+    }
+}
---
+++ src/com/cj/TestFile.java
@@ -0,0 +1,19 @@
+package com.cj;
+
+import java.io.File;
+
+public class TestFile {
+
+    public static void main(String[] args) {
+        for (int i=1;i<=381;i++)
+        {
+            File file = new File("D:\\哔哩哔哩\\83622425\\"+i);
+            Boolean flag = file.exists();
+            if(!flag)
+            {
+                System.out.println("========="+i+"=========");
+            }
+        }
+
+    }
+}
